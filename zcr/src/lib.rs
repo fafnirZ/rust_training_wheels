5b4717d03b2907dd82326b6ac52f27e7924f39ac
fafnir <gokurohono@gmail.com>
2024-09-28 23:41:38 +1000

    feat: zero copy works

--- zcr/src/lib.rs
+++ zcr/src/lib.rs
@@ -2,9 +2,11 @@
 // https://medium.com/@emreeaydiinn/zero-copy-reads-explained-8d54e6084857
 // https://blog.devgenius.io/linux-zero-copy-d61d712813fe
 
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{BufReader, BufWriter, Read, Write};
 use std::io;
+
+use memmap2::Mmap;
 
 
 // https://users.rust-lang.org/t/rust-file-open-error-handling/50681/2
@@ -34,8 +36,34 @@ pub fn normal_copy(from: &str, to: &str) -> Result<bool, io::Error> {
 }
 
 // TODO
-pub fn zero_copy() {
+pub fn zero_copy(from: &str, to: &str) -> Result<bool, io::Error> {
+    // open source file
+    let input_file = File::open(from)
+                                    .map_err(|_| io::Error::new(io::ErrorKind::NotFound, "input file not found"))?;
     
+
+    // mmap
+    // assumes there is concurrent access
+    // otherwise a mutex is required
+    let mmap = unsafe {
+        Mmap::map(&input_file)?
+    };
+    let data = &mmap[..];
+    
+    // https://stackoverflow.com/a/65703152
+    // basically a new and better way of implementing
+    // the same write functionality
+    // from the normal_copy BufWriter, etc
+    let mut new_file = fs::OpenOptions::new()
+                                .create(true) // creates new file
+                                .write(true) // allows write
+                                .open(to)?;
+
+    new_file.write_all(data)?;
+
+    Ok(true)
+
+
 }
 
 
@@ -51,8 +79,18 @@ mod tests {
         let path = env::current_dir().unwrap();
         println!("Currdir: {}", path.display());
 
+
         // testing fn
         let ok = normal_copy("seed/file.csv", "test_out/new.csv");
+        assert!(ok.is_ok());
+        let is_success = ok.unwrap();
+        assert_eq!(is_success, true);
+    }
+
+    #[test]
+    fn zero_copy_works() {
+        // testing fn
+        let ok = normal_copy("seed/file.csv", "test_out/zero_c.csv");
         assert!(ok.is_ok());
         let is_success = ok.unwrap();
         assert_eq!(is_success, true);

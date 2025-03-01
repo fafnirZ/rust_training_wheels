ad22a5b3d27636e0d25ce7bb2070e60988ab91d1
fafnir <gokurohono@gmail.com>
2025-03-01 18:11:53 +1100

    feat: adding rust code, mainly boilerplate tbh

--- pyo3_thread_bench/regex_maturin/src/lib.rs
+++ pyo3_thread_bench/regex_maturin/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use pyo3::prelude::*;
+use regex::Regex;
+
+fn rust_regex_nogil(contents: &str, regex_pattern: &str) -> Option<String> {
+    let re = Regex::new(regex_pattern).ok()?; // returns None if regex compilation fails
+    re.find(contents).map(|m| m.as_str().to_string()) // automatically returns
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[pyfunction]
+fn regex_match(py: Python<'_>, contents: &str, regex_pattern: &str) -> PyResult<Option<String>>{
+    let result = py.allow_threads(|| rust_regex_nogil(contents, regex_pattern));
+    Ok(result)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+
+#[pymodule]
+fn rust_regex(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(regex_match, m)?)?;
+    Ok(())
 }

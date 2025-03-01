b0e9eace7d1474b1cb0ccf0fc5dcf50e004ef35e
fafnir <gokurohono@gmail.com>
2025-03-01 18:53:36 +1100

    feat: changes are ACTUALLY being reflected properly.

--- pyo3_thread_bench/benchmarker/hello.py
+++ pyo3_thread_bench/benchmarker/hello.py
@@ -3,15 +3,15 @@ import regex_maturin
 
 def main():
 
-    contents = "HELLO MYNAME IS JACKY, JACKY"
-    regex_pattern = r"JACK"
+    contents = "HELLO MYNAME IS ACKY, ACKY"
+    regex_pattern = r"ACK"
 
     res = regex_maturin.regex_match(
         contents,
         regex_pattern,
     )
 
-    print(res)
+    print(type(res))
 
 
 if __name__ == "__main__":
---
+++ pyo3_thread_bench/regex_maturin/README.md
@@ -0,0 +1,11 @@
+
+building release
+```
+maturin build --release -o release
+```
+
+
+installing the release build
+```
+uv pip install `path/to/release.whl`
+```
